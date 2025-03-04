2c9551bb2771ede92a65247e7e2878e6f7b6e411
Sujal-prajapati07 <prajapatisujal1612@gmail.com>
2025-03-04 15:20:50 +0530

    Java unit 4 half completed

---
+++ sem 4/JAVA/UNIT_4/Qu1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Qu1 
+{
+  public static void main(String[] args) 
+  {
+    try 
+    {
+      System.out.println("Enter number : ");
+      Scanner scanner = new Scanner(System.in);
+
+      int number = scanner.nextInt();
+      int rem;
+      int total = 0;
+
+      while (number > 0) 
+      {
+        rem = number % 10;
+        total += rem;
+        number /= 0;
+      }
+      System.out.println("Sum of digit is : " + total);
+    } 
+    catch (Exception e) 
+    {
+      System.out.println(e);
+    }
+  }
+}
