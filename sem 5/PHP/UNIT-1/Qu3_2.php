09fe6d460e93e2c335205dd184ccd8bca8deca72
Sujal-prajapati07 <prajapatisujal1612@gmail.com>
2025-08-07 11:42:44 +0530

    PHP removed

--- sem 5/PHP/UNIT-1/Qu3_2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$String1 = $_POST['s1']; 
-	$String2 = $_POST['s2'];
-	$String3 =	$_POST['s3'];
-
-	echo "<h1>$String1"."$String2"."$String3</h1><br><br>";
-	print("<h1>$String1"."$String2"."$String3</h1>");
-?>
